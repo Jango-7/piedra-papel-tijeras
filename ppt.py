5bc5652ea824869a25c878ad1432c59034ef6542
Jango-7 <jhoanngo2@gmail.com>
2025-01-30 22:22:23 -0300

    Reversion del juego

--- ppt.py
+++ ppt.py
@@ -1,16 +1,37 @@
+# Desafío
+# Se puede colocar el nombre en la segunda solicitud
+# Lo ingresado por el usuario sea lowerCase de tal forma de comparar minúscula con minúscula
+# Más de 1 turno con el bucle while
+
 nombre1 = input('Nombre jugador 1: ')
 nombre2 = input('Nombre jugador 2: ')
 
-jugador1 = input('Hola ' + nombre1 + ' qué eliges: piedra, papel o tijera? ')
-jugador2 = input('Hola ' + nombre2 + ' qué eliges: piedra, papel o tijera? ')
+victorias1 = 0
+victorias2 = 0
 
-condicion1 = jugador1 == 'piedra' and jugador2 == 'tijera'
-condicion2 = jugador1 == 'papel' and jugador2 == 'piedra'
-condicion3 = jugador1 == 'tijera' and jugador2 == 'papel'
+while victorias1 < 3 and victorias2 < 3:
 
-if condicion1 or condicion2 or condicion3:
-    print('Felicitaciones', nombre1, 'has ganado!')
-elif jugador1 == jugador2:
-    print('Ha habido un empate')
+
+    jugador1 = input(f'Hola {nombre1}, qué eliges: piedra, papel o tijera? ').lower()
+    jugador2 = input(f'Hola {nombre2}, qué eliges: piedra, papel o tijera? ').lower()
+
+    condicion1 = jugador1 == 'piedra' and jugador2 == 'tijera'
+    condicion2 = jugador1 == 'papel' and jugador2 == 'piedra'
+    condicion3 = jugador1 == 'tijera' and jugador2 == 'papel'
+    
+    if jugador1 == jugador2:
+        print('Hay un empate')
+    elif condicion1 or condicion2 or condicion3:
+        print(f'{nombre1} has ganado!')
+        victorias1 += 1
+    else:
+        print(f'{nombre2}, has ganado!')
+        victorias2 += 1
+    
+    print(f'Marcador: {nombre1} {victorias1} - {victorias2} {nombre2}')
+    print('-' * 30) 
+
+if victorias1 == 3:
+    print(f'{nombre1} has ganado con 3 victorias')
 else:
-    print('Felicidades', nombre2, 'has ganado!')
+    print(f'{nombre2} has ganado con 3 victorias')
